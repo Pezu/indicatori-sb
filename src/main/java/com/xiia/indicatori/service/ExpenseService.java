7e11716926bdeef669d0960b2fd91a0bdccbe0b2
Radu <rpaise@gmail.com>
2018-11-27 12:31:54 +0200

    expenses filter

--- src/main/java/com/xiia/indicatori/service/ExpenseService.java
+++ src/main/java/com/xiia/indicatori/service/ExpenseService.java
@@ -5,6 +5,10 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
 import org.springframework.stereotype.Service;
 
 import com.xiia.indicatori.domain.Expense;
@@ -17,10 +21,13 @@ import com.xiia.indicatori.repositories.RepositoryRegistry;
 @Service
 public class ExpenseService {
 	
+	@PersistenceContext
+    private EntityManager entityManager;
     private final RepositoryRegistry repositoryRegistry;
     
-    public ExpenseService(final RepositoryRegistry repositoryRegistry) {
+    public ExpenseService(final RepositoryRegistry repositoryRegistry, final EntityManager entityManager) {
     	this.repositoryRegistry = repositoryRegistry;
+    	this.entityManager = entityManager;
 	}
 
 	public List<SplitPercentage> getPercentages(Integer article, Integer unit) {
@@ -61,7 +68,34 @@ public class ExpenseService {
 		repositoryRegistry.getExpensesRepository().deleteById(expenseId);
 	}
 
-	public List<Expense> getAllExpenses(ExpenseFilter filter) {
+	public List<Expense> getAllExpenses() {
 		return repositoryRegistry.getExpensesRepository().findAll();
 	}
+	public List<Expense> getAllExpenses(ExpenseFilter filter) {
+		StringBuilder sb = new StringBuilder("select * from expenses where month = '");
+		sb.append(filter.getMonth());
+		sb.append("'");
+		if (filter.getUnitId() != null) {
+			sb.append(" and unit_id = ");
+			sb.append(filter.getUnitId());
+		}
+		if (filter.getGroupId() != null) {
+			sb.append(" and group_id = ");
+			sb.append(filter.getGroupId());
+		}
+		if (filter.getCategoryId() != null) {
+			sb.append(" and category_id = ");
+			sb.append(filter.getCategoryId());
+		}
+		if (filter.getArticleId() != null) {
+			sb.append(" and article_id = ");
+			sb.append(filter.getArticleId());
+		}
+        Query query = entityManager.createNativeQuery(sb.toString());
+        List<Expense> expenses = new ArrayList<Expense>();
+        for (Object expense : query.getResultList()) {
+        	expenses.add((Expense)expense);
+        }
+        return expenses;
+    }
 }
